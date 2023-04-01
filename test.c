3132d7ad285cf40230c59188d2d5306e31a5949a
SV1Stail <66378412+SV1Stail@users.noreply.github.com>
2023-04-01 20:30:12 +0700

    231

--- test.c
+++ test.c
@@ -1,11 +1,9 @@
-#include "polish_notation.h"
-
-#include <math.h>
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
-
+#include <math.h>
 #include "stack.h"
+#include "polish_notation.h"
 
 int get_priority(char c) {
     int flag = 0;
@@ -35,37 +33,34 @@ int get_priority(char c) {
     return flag;
 }
 
-stack *get_polish(stack *input_list) {
-    double d = 0;
-    char a = '\0';
-    stack *stack_list = NULL;
-    stack *output_list = NULL;
+stack * get_polish(stack * input_list) {
+    double d = 0; char a  = '\0';
+    stack * stack_list = NULL; stack * output_list = NULL;
     if (input_list != NULL) {
         stack *ptr = input_list;
         while (ptr != NULL) {
             if (ptr->action == 'd' || ptr->action == 'x') {
-                push_stack(&output_list, ptr->action, ptr->digit);
+                push_stack(&output_list,  ptr->action, ptr->digit);
             } else if (strchr(IN_STACK, ptr->action) != NULL) {
-                push_stack(&stack_list, ptr->action, ptr->digit);
+                push_stack(&stack_list,  ptr->action, ptr->digit);
             } else if (ptr->action == ')') {
                 if (stack_list != NULL) {
                     do {
                         pop_stack(&stack_list, &a, &d);
                         if (a != '(') {
-                            push_stack(&output_list, a, d);
+                            push_stack(&output_list,  a, d);
                         }
                     } while (a != '(' && stack_list != NULL);
                     if (stack_list != NULL && strchr(IN_FUNC, stack_list->action) != NULL) {
                         pop_stack(&stack_list, &a, &d);
-                        push_stack(&output_list, a, d);
+                    push_stack(&output_list,  a, d);
                     }
                 }
             } else if (strchr(IN_OPER, ptr->action) != NULL) {
                 if (stack_list != NULL) {
                     while (stack_list != NULL &&
                            get_priority(stack_list->action) >= get_priority(ptr->action)) {
-                        pop_stack(&stack_list, &a, &d);
-                        push_stack(&output_list, a, d);
+                        pop_stack(&stack_list, &a, &d); push_stack(&output_list,  a, d);
                     }
                 }
                 push_stack(&stack_list, ptr->action, ptr->digit);
@@ -73,77 +68,39 @@ stack *get_polish(stack *input_list) {
             ptr = ptr->next;
         }
         while (stack_list != NULL) {
-            pop_stack(&stack_list, &a, &d);
-            push_stack(&output_list, a, d);
+            pop_stack(&stack_list, &a, &d); push_stack(&output_list,  a, d);
         }
     }
     destroy_stack(&stack_list);
     return output_list;
 }
 
-double get_stack_value(stack *input_list, double x, int *flag) {
-    stack *stack_list = NULL;
-    stack *ptr = input_list;
+double get_stack_value(stack * input_list, double x, int *flag) {
+    stack * stack_list = NULL;
+    stack * ptr = input_list;
     double d = 0;
     double d2 = 0;
-    char a = '\0';
+    char a  = '\0';
     while (ptr != NULL) {
         switch (ptr->action) {
-            case 'd':
-                push_stack(&stack_list, 'd', ptr->digit);
-                break;
-            case 'x':
-                push_stack(&stack_list, 'x', x);
-                break;
-            case '+':
-                pop_stack(&stack_list, &a, &d);
-                pop_stack(&stack_list, &a, &d2);
-                push_stack(&stack_list, 'd', d2 + d);
-                break;
-            case '*':
-                pop_stack(&stack_list, &a, &d);
-                pop_stack(&stack_list, &a, &d2);
-                push_stack(&stack_list, 'd', d2 * d);
-                break;
-            case '/':
-                pop_stack(&stack_list, &a, &d);
-                pop_stack(&stack_list, &a, &d2);
-                push_stack(&stack_list, 'd', d2 / d);
-                break;
-            case '-':
-                pop_stack(&stack_list, &a, &d);
-                pop_stack(&stack_list, &a, &d2);
-                push_stack(&stack_list, 'd', d2 - d);
-                break;
-            case '^':
-                pop_stack(&stack_list, &a, &d);
-                pop_stack(&stack_list, &a, &d2);
-                push_stack(&stack_list, 'd', pow(d2, d));
-                break;
-            case 's':
-                pop_stack(&stack_list, &a, &d);
-                push_stack(&stack_list, 'd', sin(d));
-                break;
-            case 'c':
-                pop_stack(&stack_list, &a, &d);
-                push_stack(&stack_list, 'd', cos(d));
-                break;
-            case 't':
-                pop_stack(&stack_list, &a, &d);
-                push_stack(&stack_list, 'd', tan(d));
-                break;
-            case 'g':
-                pop_stack(&stack_list, &a, &d);
-                push_stack(&stack_list, 'd', 1 / tan(d));
-                break;
-            case 'q':
-                pop_stack(&stack_list, &a, &d);
-                push_stack(&stack_list, 'd', sqrt(d));
-                break;
-            case 'l':
-                pop_stack(&stack_list, &a, &d);
-                push_stack(&stack_list, 'd', log(d));
-                break;
+            case 'd': push_stack(&stack_list,  'd', ptr->digit); break;
+            case 'x': push_stack(&stack_list,  'x', x); break;
+            case '+': pop_stack(&stack_list, &a, &d); pop_stack(&stack_list, &a, &d2);
+                push_stack(&stack_list,  'd', d2+d); break;
+            case '*': pop_stack(&stack_list, &a, &d);  pop_stack(&stack_list, &a, &d2);
+                push_stack(&stack_list,  'd', d2*d);  break;
+            case '/': pop_stack(&stack_list, &a, &d); pop_stack(&stack_list, &a, &d2);
+                push_stack(&stack_list,  'd', d2/d); break;
+            case '-': pop_stack(&stack_list, &a, &d); pop_stack(&stack_list, &a, &d2);
+                push_stack(&stack_list, 'd', d2-d); break;
+            case '^': pop_stack(&stack_list, &a, &d); pop_stack(&stack_list, &a, &d2);
+                push_stack(&stack_list, 'd', pow(d2, d)); break;
+            case 's': pop_stack(&stack_list, &a, &d); push_stack(&stack_list, 'd', sin(d)); break;
+            case 'c': pop_stack(&stack_list, &a, &d); push_stack(&stack_list, 'd', cos(d));  break;
+            case 't': pop_stack(&stack_list, &a, &d); push_stack(&stack_list, 'd', tan(d));  break;
+            case 'g': pop_stack(&stack_list, &a, &d); push_stack(&stack_list, 'd', 1/tan(d)); break;
+            case 'q': pop_stack(&stack_list, &a, &d); push_stack(&stack_list, 'd', sqrt(d)); break;
+            case 'l': pop_stack(&stack_list, &a, &d); push_stack(&stack_list, 'd', log(d)); break;
         }
         ptr = ptr->next;
     }
